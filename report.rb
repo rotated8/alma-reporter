5964aa89fa67f830c828f611be1b5abfa9e39a52
Collin Brittle <cnbrittle@gmail.com>
2021-04-16 03:11:58 -0400

    Adds logging for the report generation.
    Previously, there was no way of watching the script progress.

--- report.rb
+++ report.rb
@@ -9,15 +9,19 @@ solr = RSolr.connect(url: SOLR_URL)
 
 # Query all fields, return zero as CSV to get headers. These are all fields present in at least one Solr doc.
 fields = solr.get('select', params: {q: '*:*', rows: 0, wt: 'csv'}).strip().split(',').sort!
+puts "@ got fields"
 # Only select fields ending in `_isi` to get counts but no data.
 fields.select! { |f| f.end_with?('_isi') }
 # Move the leader to the front, just because
 fields.delete('l_ldr_isi')
 fields.prepend('l_ldr_isi')
+puts "@ ordered fields"
 
 headers = ['field', 'docs', 'occurs']
 CSV.open('./data.csv', 'w', write_headers: true, headers: headers) do |csv|
   for field in fields
+    puts "@ #{Time.now}"
+    puts "@ getting #{field}"
     # Get count and sum stats for this field.
     resp = solr.get('select', params: { rows: 0, stats: true, 'stats.field': "{!count=true sum=true}#{field}" })
     # This is the number of documents the field occurs in
